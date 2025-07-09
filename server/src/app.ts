88322693197e5eb3469b01cf79821c5a6edab5c3
deeoak sharma <demondark7678@gmail.com>
2025-07-09 09:42:18 +0530

    Added Next.js and dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: function (origin, callback) {
+    callback(null, true); // allow all origins
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 app.use(
